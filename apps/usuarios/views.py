1f8c9f053e438bb21e15504c32d466143bef6227
Wesley Diorrani Ferreira <wesleydf@weg.net>
2026-02-10 16:18:37 -0300

    Add CRUD

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from usuarios.forms import LoginForms
-from usuarios.forms import CadastroForms
+from apps.usuarios.forms import LoginForms
+from apps.usuarios.forms import CadastroForms
 from django.contrib.auth.models import User
 from django.contrib import auth
 from django.contrib import messages
